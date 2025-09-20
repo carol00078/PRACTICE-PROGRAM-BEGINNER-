1a289c474915af8589605fe3164986ab39f9aea2
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-20 18:52:55 +0500

    Add files via upload

---
+++ Addition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Add two numbers"<<endl;
+  int a=10;
+  int b=20;
+    int sum= a+b;
+    cout<<"Sum= "<<sum;
+  
+    return 0; 
+}
