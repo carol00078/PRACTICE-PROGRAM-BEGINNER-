b14e23962475a4bf421201774cf4ae7f52300d34
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-20 18:53:50 +0500

    Add files via upload

---
+++ difference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Difference of number"<<endl;
+      int a=50;
+      int b=10;
+       int Sum=a-b;
+       cout<<"Sum="<<Sum<<endl;
+
+        return 0; 
+}
