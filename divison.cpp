b70450addaa7b7602a2b0a8eb327a6c57821ad52
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-20 18:54:41 +0500

    Add files via upload

---
+++ divison.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Divide two numbers"<<endl;
+    int a=50;
+    int b=20;
+    int Sum=a/b;
+      cout<<"Sum="<<Sum;
+        return 0;
+        
+}
