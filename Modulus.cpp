f690edf27c9dfc73f71bd110cce573beca3c5bec
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-23 16:40:08 +0500

    Add files via upload

---
+++ Modulus.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Modulus of Numbers"<<endl;
+    int a=80;
+    int b=20;
+    int Sum=a%b;
+      cout<<"Sum="<<Sum;
+      return 0;
+}
