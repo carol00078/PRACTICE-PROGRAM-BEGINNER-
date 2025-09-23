f9efb167b640420d148f668833c648aacff59a61
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-23 16:40:53 +0500

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Product of numbers"<<endl;
+    int a=10;
+    int b=20;
+    int Sum=a*b;
+    cout<<"Sum="<<Sum;
+     return 0;
+}
