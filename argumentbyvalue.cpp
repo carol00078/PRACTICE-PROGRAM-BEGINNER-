14cdc533b37d6481bf5ceb34a2453dc852210afd
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-25 13:49:55 +0500

    Add files via upload

---
+++ argumentbyvalue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int sq(int a){
+    a=a*a;
+}
+int main(){
+    int a =2;
+    //cout<<"ENTER VALUE"<<endl;
+    //cin>>a;
+    int result=sq(2);
+    cout<<"change "<<result<<endl;
+    cout<<"original "<<a<<endl;
+
+return 0;
+}
