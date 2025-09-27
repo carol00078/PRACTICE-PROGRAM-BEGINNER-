c0e85d72f362656f8bb521266793406df4f2dd41
Codez e Carol <carol.sweety24@yahoo.com>
2025-09-27 18:18:20 +0500

    Add files via upload

---
+++ callbyreference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int change(int &a){
+    a=a+5;
+    cout<<"INSIDE"<<a<<endl;
+}
+int main(){
+   int a=10;
+   change(a);
+   cout<<"OUTSIDE"<<a<<endl;
+return 0;
+}
